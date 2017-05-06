96fd25ea30ec252b7868519404a92406d4e14862
Jose Martins <lluismmartins7@gmail.com>
2017-05-06 18:26:21 +0100

    very cutchi cutchi

--- src/src/weka/App.java
+++ src/src/weka/App.java
@@ -1,8 +1,6 @@
 package weka;
 
-import sun.reflect.generics.tree.Tree;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
 import weka.gui.treevisualizer.PlaceNode2;
 import weka.gui.treevisualizer.TreeVisualizer;
 
@@ -10,7 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
 
 public class App {
     private JPanel panel;
@@ -33,11 +30,12 @@ public class App {
         init();
 
         try {
-            year1Button.addActionListener(new buttonHandler(handler.determineTree(1) ,handler.getTree(1).graph()));
-            year2Button.addActionListener(new buttonHandler(handler.determineTree(2) ,handler.getTree(2).graph()));
-            year3Button.addActionListener(new buttonHandler(handler.determineTree(3) ,handler.getTree(3).graph()));
-            year4Button.addActionListener(new buttonHandler(handler.determineTree(4) ,handler.getTree(4).graph()));
-            year5Button.addActionListener(new buttonHandler(handler.determineTree(5) ,handler.getTree(5).graph()));
+
+            year1Button.addActionListener(new buttonHandler(handler.determineTree(1) ,handler.getTree(1)));
+            year2Button.addActionListener(new buttonHandler(handler.determineTree(2) ,handler.getTree(2)));
+            year3Button.addActionListener(new buttonHandler(handler.determineTree(3) ,handler.getTree(3)));
+            year4Button.addActionListener(new buttonHandler(handler.determineTree(4) ,handler.getTree(4)));
+            year5Button.addActionListener(new buttonHandler(handler.determineTree(5) ,handler.getTree(5)));
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,36 +46,53 @@ public class App {
     public class buttonHandler implements ActionListener {
 
         String msg;
-        String tree;
+        J48 tree;
 
-        public buttonHandler(String msg, String tree) {
+        public buttonHandler(String msg, J48 tree) {
             this.msg = msg;
             this.tree = tree;
         }
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
+            try {
             // display classifier
             final javax.swing.JFrame jf = new javax.swing.JFrame("Tree view");
-            jf.setSize(1920,1080);
-            jf.getContentPane().setLayout(new BorderLayout());
-            TreeVisualizer tv = null;
-            tv = new TreeVisualizer(null,tree, new PlaceNode2());
+                jf.setSize(1920,1080);
+                jf.getContentPane().setLayout(new BorderLayout());
+                TreeVisualizer tv = null;
 
-            jf.getContentPane().add(tv, BorderLayout.CENTER);
-            jf.addWindowListener(new java.awt.event.WindowAdapter() {
-                public void windowClosing(java.awt.event.WindowEvent e) {
-                    jf.dispose();
-                }
-            });
+                //set option
+                String[] options = new String[1];
 
-            jf.setVisible(true);
-            tv.fitToScreen();
 
-            log.setText(null);
-            log.append(msg);
-            log.append("Fiability of all years");
-            log.append(handler.toString());
+                if(unprunedCheckBox.isSelected())
+                    options[0] = "-U";
+                else
+                    options[0] = "";
+
+                tree.setOptions(options);
+
+                tv = new TreeVisualizer(null,tree.graph(), new PlaceNode2());
+
+                jf.getContentPane().add(tv, BorderLayout.CENTER);
+                jf.addWindowListener(new java.awt.event.WindowAdapter() {
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        jf.dispose();
+                    }
+                });
+
+                jf.setVisible(true);
+                tv.fitToScreen();
+
+                log.setText(null);
+                log.append(msg);
+                log.append("Fiability of all years");
+                log.append(handler.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
